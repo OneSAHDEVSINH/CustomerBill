22cc120451e7d30fd38f06d25f8f8ba2f3285fd3
SAHDEVSINH PARMAR <sahdevsinhparmar1753@gmail.com>
2024-10-17 20:20:00 +0530

    Add project files.

---
+++ CustomerBill/Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerBill
+{
+    internal class Customer
+    {
+        private string _name;
+        private int _units;
+        private float _totalBill, _remains;
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public int Units
+        {
+            set
+            {
+                _units = value;
+            }
+        }
+
+        public float Remains
+        {
+            set
+            {
+                _remains = value;
+            }
+        }
+
+        public void CalculateBill()
+        {
+            if (_units <= 100)
+            {
+                _totalBill = _units * 2.5f;
+            }
+            else
+            {
+                _totalBill = 100 * 2.5f;
+                _remains = _units - 100;
+                if (_remains <= 200)
+                {
+                    _totalBill += _remains * 3.5f;
+                }
+                else
+                {
+                    _totalBill = 200 * 3.5f;
+                    _remains -= 200;
+                    _totalBill += _remains * 4.5f;
+                    
+                }
+            }
+        }
+
+        public Tuple<string, int, float, float> GetData()
+        {
+            return new Tuple<string, int, float, float>(_name, _units, _remains, _totalBill);
+        }
+    }
+}
---
+++ CustomerBill/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerBill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cs = new Customer();
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            cs.SetName(name);
+
+            Console.Write("Enter units consumed: ");
+            int units = Convert.ToInt32(Console.ReadLine());
+            cs.Units = units;
+
+            cs.CalculateBill();
+
+            Tuple<string, int, float, float> result = cs.GetData();
+
+            Console.WriteLine("\nGenarated Bill::--");
+            Console.WriteLine("Name: " + result.Item1);
+            Console.WriteLine("Units: " + result.Item2);
+            Console.WriteLine("Charges: " + result.Item3);
+            Console.WriteLine("Total Bill: " + result.Item4);
+
+            Console.ReadKey();
+        }
+    }
+}
